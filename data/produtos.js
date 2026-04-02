f32c8b90050a9229f455f143c5131a19b8a17f2a
Vitor Gabriel <vitorlatero03@gmail.com>
2026-04-02 00:11:34 -0300

    Adicionano Footer

--- data/produtos.js
+++ data/produtos.js
@@ -18,5 +18,100 @@ const produtos = [
       "assets/img/produtos/produto2-1.jpg",
       "assets/img/produtos/produto2-2.jpg"
     ]
+  },
+ {
+    nome: "Rasteirinha Confort",
+    categoria: "rasteirinha",
+    material: "Borracha",
+    numeracao: "33 ao 38",
+    imagens: [
+      "assets/img/produtos/produto2-1.jpg",
+      "assets/img/produtos/produto2-2.jpg"
+    ]
+  }, 
+  {
+    nome: "Rasteirinha Confort",
+    categoria: "rasteirinha",
+    material: "Borracha",
+    numeracao: "33 ao 38",
+    imagens: [
+      "assets/img/produtos/produto2-1.jpg",
+      "assets/img/produtos/produto2-2.jpg"
+    ]
+  },
+   {
+    nome: "Rasteirinha Confort",
+    categoria: "rasteirinha",
+    material: "Borracha",
+    numeracao: "33 ao 38",
+    imagens: [
+      "assets/img/produtos/produto2-1.jpg",
+      "assets/img/produtos/produto2-2.jpg"
+    ]
+  },
+   {
+    nome: "Rasteirinha Confort",
+    categoria: "rasteirinha",
+    material: "Borracha",
+    numeracao: "33 ao 38",
+    imagens: [
+      "assets/img/produtos/produto2-1.jpg",
+      "assets/img/produtos/produto2-2.jpg"
+    ]
+  },
+   {
+    nome: "Rasteirinha Confort",
+    categoria: "rasteirinha",
+    material: "Borracha",
+    numeracao: "33 ao 38",
+    imagens: [
+      "assets/img/produtos/produto2-1.jpg",
+      "assets/img/produtos/produto2-2.jpg"
+    ]
+  }, 
+  {
+    nome: "Rasteirinha Confort",
+    categoria: "rasteirinha",
+    material: "Borracha",
+    numeracao: "33 ao 38",
+    imagens: [
+      "assets/img/produtos/produto2-1.jpg",
+      "assets/img/produtos/produto2-2.jpg"
+    ]
   }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 ];
